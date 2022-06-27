25355cb84e1cb6853ccedcec968f37054efc9c94
Cecília <60898302+ninth-in@users.noreply.github.com>
2022-06-27 13:58:02 -0300

    adding the reader

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,13 +1,31 @@
 package com.example;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+import java.io.IOException;
+import java.util.Arrays;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String line = "";
+        int aux = 0;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("Dados-Pedro/source/products_shuffled.csv"));
+            while((line = br.readLine()) != null){
+                String[] values = line.split(",");
+                if(values[0] == "0001"){
+                    aux = aux + 1; 
+                }
+                
+            }
+            System.out.println(aux);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
